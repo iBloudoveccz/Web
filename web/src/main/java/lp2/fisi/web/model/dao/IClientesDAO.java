40546658033520c904dc69466091b71087824fdc
Carlos Gabriel Rios Caro <67565173+iBloudoveccz@users.noreply.github.com>
2024-06-25 16:37:03 -0500

    Delete web directory

--- web/src/main/java/lp2/fisi/web/model/dao/IClientesDAO.java
+++
@@ -1,8 +0,0 @@
-package lp2.fisi.web.model.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import lp2.fisi.web.model.Clientes;
-
-public interface IClientesDAO extends JpaRepository<Clientes, Integer>{
-
-}
